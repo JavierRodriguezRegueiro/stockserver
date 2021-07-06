59d961ff9fe7124441a447a749125a046c8fe782
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2021-07-06 16:32:14 +0200

    Initial commit

---
+++ src/main/java/com/stock/server/StockserverApplication.java
@@ -0,0 +1,13 @@
+package com.stock.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockserverApplication.class, args);
+	}
+
+}
