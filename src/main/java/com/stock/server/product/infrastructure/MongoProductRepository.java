9a741f5fc024a318dc546e219f5c969ad76742ae
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2021-07-31 19:13:21 +0200

    Remove unused List import

--- src/main/java/com/stock/server/product/infrastructure/MongoProductRepository.java
+++ src/main/java/com/stock/server/product/infrastructure/MongoProductRepository.java
@@ -9,9 +9,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class MongoProductRepository implements ProductRepository {
